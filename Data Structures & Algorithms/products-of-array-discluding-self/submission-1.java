fef29426e00884cb744091da12acfb31e2b45f8d
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2026-04-10 17:10:10 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        res[0] = 1;
+        for(int i=1;i<len;i++){
+            res[i] = res[i-1]*nums[i-1];
+        }
+        int rightProduct=nums[len-1];
+        for(int i=len-2;i>=0;i--){
+            res[i] = res[i]*rightProduct;
+            rightProduct = rightProduct *nums[i];
+        }
+        return res;
+    }
+}  
