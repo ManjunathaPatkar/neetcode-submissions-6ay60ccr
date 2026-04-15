8b4abce034aa9197a9ec57e117bd7912a6d0ff13
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2026-04-15 13:01:02 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> cur =new ArrayList<>();
+        helper(res, cur, nums, new HashSet<>());
+        return res;
+    }
+
+    private void helper(
+         List<List<Integer>> res, List<Integer> cur, int[] nums,
+         HashSet<Integer> hs
+    ){
+        if(cur.size()==nums.length){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int ind =0;ind<nums.length;ind++){
+            if(hs.contains(nums[ind])){
+                continue;
+            }
+            hs.add(nums[ind]);
+            cur.add(nums[ind]);
+            helper(res,cur,nums,hs);
+            hs.remove(cur.get(cur.size()-1));
+            cur.remove(cur.size()-1);
+        }
+    }
+}
+
