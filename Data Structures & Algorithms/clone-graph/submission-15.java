34f42803e9c6a3fa639337e2e3a3db71b315bb54
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2026-04-15 23:03:35 +0530

    Add: clone-graph - submission-15

---
+++ Data Structures & Algorithms/clone-graph/submission-15.java
@@ -0,0 +1,43 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node==null){
+            return null;
+        }
+        HashMap<Node,Node> hm = new HashMap<>();
+        Queue<Node> q =new LinkedList<>();
+        q.offer(node);
+        hm.put(node,new Node(node.val));
+        while(!q.isEmpty()){
+            Node popped = q.poll();
+            
+            for(Node neighbor: popped.neighbors){
+                if(!hm.containsKey(neighbor)){
+                    hm.put(neighbor,new Node(neighbor.val));
+                    q.offer(neighbor);
+                }
+                hm.get(popped).neighbors.add(hm.get(neighbor));
+            }
+        }
+        return hm.get(node);
+    }
+}
