c224b6a6b72bf0d930a9174fd92ce61756f7cbd5
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2026-04-16 21:26:25 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        boolean[] vis = new boolean[n];
+        List<Integer>[] graph = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            graph[i]=new ArrayList<>();
+        }
+        for(int[] edge:edges){
+            graph[edge[1]].add(edge[0]);
+            graph[edge[0]].add(edge[1]);
+        }
+        if(!dfs(graph,0,-1,vis)){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(List<Integer>[] graph,int i,int parent, boolean[] vis){
+        vis[i]=true;
+        for(int adj:graph[i]){
+            if(!vis[adj]){
+                if(!dfs(graph,adj,i,vis)){
+                    return false;
+                }
+            }
+            else if(adj!=parent){
+                return false;
+            }
+        }
+        return true;
+    }
+}
